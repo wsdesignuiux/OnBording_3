5001ebd36f4a892da988ea5f401b0760eb0578d1
Rushabh Patel <wsdesignuiux@gmail.com>
2018-12-31 10:35:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wolfsoft3/onbording_3/OnBording_3.java
@@ -0,0 +1,13 @@
+package com.example.wolfsoft3.onbording_3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class OnBording_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.on_bording_3);
+    }
+}
